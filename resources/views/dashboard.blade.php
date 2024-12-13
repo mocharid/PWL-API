490e388136e687d1de85fb87be4a04ab46090f99
mocharid <ariddd.edu@gmail.com>
2024-12-13 13:08:58 +0700

    ubah posisi

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,12 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Selamat Datang, ".Auth()->user()->name." !") }}
+            <div class="dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100 flex flex-col items-center">
+                    <!-- Selamat Datang Text -->
+                    <p class="mb-4 font-bold">{{ __("Selamat Datang, ".Auth()->user()->name." !") }}</p>
+                    <!-- Image -->
+                    <img src="{{ asset('images/b.png') }}" alt="Image Description" class="rounded-lg shadow-md">
                 </div>
             </div>
         </div>
